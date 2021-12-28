d758b59c366166917056efc4b91c883e12fce4b3
Lucas Santos <lucas201273@gmail.com>
2021-12-27 22:46:02 -0300

    feat: improved usability on signin method

--- src/app/pages/SignIn/index.tsx
+++ src/app/pages/SignIn/index.tsx
@@ -1,13 +1,19 @@
-import React, { useEffect, useState, useCallback } from 'react'
+import React, { useEffect, useState, useCallback, useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 import { motion } from 'framer-motion'
-import logo from '../../assets/img/logo.svg'
 import { Container, Content, Form } from './styles'
+import { UserContext } from '../../hooks/UserContext'
+
+import logo from '../../assets/img/logo.svg'
 
 export function SignIn () {
+  const navigate = useNavigate()
   const [text, setText] = useState('')
   const [hasChange, setHasChange] = useState(false)
   const [isDisabled, setIsDisabled] = useState(true)
-
+  const [isConfirmed, setIsConfirmed] = useState(false)
+  const { handleSaveData } = useContext(UserContext)
   const handleValidateName = useCallback((value: string) => {
     if (value.length >= 3) {
       setIsDisabled(false)
@@ -18,10 +24,28 @@ export function SignIn () {
     }
   }, [text])
 
+  const handleNavigate = () => {
+    handleValidateName(text)
+    handleSaveData(text)
+    setIsConfirmed(true)
+    setTimeout(() => {
+      navigate('dashboard')
+    }, 1000)
+    setText('')
+    toast.success(`Seja Bem-Vindo ${text}!`)
+  }
   useEffect(() => {
     handleValidateName(text)
   }, [text])
+  /*
+10
 
+var dt = new Date(); // current date of week
+var currentWeekDay = dt.getDay();
+var lessDays = currentWeekDay == 0 ? 6 : currentWeekDay - 1;
+var wkStart = new Date(new Date(dt).setDate(dt.getDate() - lessDays));
+var wkEnd = new Date(new Date(wkStart).setDate(wkStart.getDate() + 6));
+*/
   return (
     <Container changeArea={hasChange}>
             <motion.div
@@ -33,8 +57,16 @@ export function SignIn () {
 
               }}
               animate={{ y: [100, 0] }}
-              transition={{ ease: 'easeOut', duration: 1 }}
+              transition={{ ease: 'easeOut', duration: 1.5 }}
             >
+        {isConfirmed
+          ? (
+
+          <motion.div
+            animate={{ opacity: 0 }}
+            transition ={{ ease: 'easeOut', duration: 0.5 }}
+          >
+
         <Content>
             <img src={logo} alt="logo do projeto"/>
             <span> Seja  bem vindo ao estudo de redesign da interface do Banco Inter</span>
@@ -43,15 +75,38 @@ export function SignIn () {
             <input
              type="text"
              value={text}
-             placeholder="Informe seu nome para entrar"
+             placeholder="Digite seu nome para entrar"
              onChange={(e) => setText(e.target.value)}
              />
              <button
               type="button"
               disabled={isDisabled}
+              onClick={handleNavigate}
               > Entrar no App</button>
              </Form>
         </Content>
+        </motion.div>
+            )
+          : (
+          <Content>
+          <img src={logo} alt="logo do projeto"/>
+          <span> Seja  bem vindo ao estudo de redesign da interface do Banco Inter</span>
+
+          <Form disabled={isDisabled}>
+          <input
+           type="text"
+           value={text}
+           placeholder="Digite seu nome para entrar"
+           onChange={(e) => setText(e.target.value)}
+           />
+           <button
+            type="button"
+            disabled={isDisabled}
+            onClick={handleNavigate}
+            > Entrar no App</button>
+           </Form>
+      </Content>
+            )}
             </motion.div>
 
     </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { AppProvider } from './app/hooks/UserContext'
 import { App } from './app/components'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
