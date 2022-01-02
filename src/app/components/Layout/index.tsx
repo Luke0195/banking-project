4685f3621de8a9602c61abe1c8f7382a69d4cdd1
Lucas Santos <lucas201273@gmail.com>
2022-01-02 18:28:17 -0300

    feat: move all files to a single component

---
+++ src/app/components/Layout/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Header } from '../index'
+import { Container } from './styles'
+
+export function Layout () {
+  return (
+    <Container>
+      <Header/>
+    </Container>
+  )
+}
--- src/app/pages/Dashboard/index.tsx
+++ src/app/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Header } from '../../components/Header'
+import { Layout } from '../../components'
 export function Dashboard () {
   return (
-    <Header/>
+    <Layout/>
   )
 }
